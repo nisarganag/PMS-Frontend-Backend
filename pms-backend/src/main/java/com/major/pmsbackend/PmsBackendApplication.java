ac4bdb8d30420df10af3cb5b8c786392e52e6f81
nisarganag <nisarga.nag.cse24@heritageit.edu.in>
2024-02-13 12:07:02 +0530

    Latest

---
+++ pms-backend/src/main/java/com/major/pmsbackend/PmsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.major.pmsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PmsBackendApplication.class, args);
+	}
+
+}
