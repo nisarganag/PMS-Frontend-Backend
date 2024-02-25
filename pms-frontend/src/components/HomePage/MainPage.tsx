27c7378b37e4e312bb49f34cce55f978f321ed7e
nisarganag <nisarga.nag.cse24@heritageit.edu.in>
2024-02-25 12:17:17 +0530

    Latest

--- pms-frontend/src/components/HomePage/MainPage.tsx
+++ pms-frontend/src/components/HomePage/MainPage.tsx
@@ -1,5 +1,5 @@
 import { Component } from 'react';
-
+import "./MainPage.css";
 class MainPage extends Component {
     constructor(props: object | Readonly<object>) {
         super(props);
@@ -9,8 +9,12 @@ class MainPage extends Component {
     }
   render() {
     return (
-      <div>
-        MainPage
+      <div className='mainPage_component'>
+        <div className="mainPage_selector"></div>
+        <div className="mainPage_searchBox">
+          <input type="text" placeholder='Search'/>
+        </div>
+        <div className="mainPage_searchButton"></div>
       </div>
     );
   }
