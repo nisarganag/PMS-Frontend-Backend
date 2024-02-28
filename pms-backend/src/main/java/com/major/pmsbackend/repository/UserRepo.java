8d22b919067a94b05ee704169a5c32b24066900a
nisarganag <nisarga.nag.cse24@heritageit.edu.in>
2024-02-28 16:28:20 +0530

    Latest

--- pms-backend/src/main/java/com/major/pmsbackend/repository/UserRepo.java
+++ pms-backend/src/main/java/com/major/pmsbackend/repository/UserRepo.java
@@ -1,10 +1,11 @@
 package com.major.pmsbackend.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.major.pmsbackend.entity.Users;
 
-
+@Repository
 public interface UserRepo extends CrudRepository<Users, Long>{
     @SuppressWarnings({ "null", "unchecked" })
     Users save(Users user);
