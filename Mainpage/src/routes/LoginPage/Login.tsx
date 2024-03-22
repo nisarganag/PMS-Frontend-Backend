243a7d1cc25900afae1ecca880b70c620d9d8c79
sutanuadhikary <sutanu.adhikary.8131@gmail.com>
2024-03-23 00:49:43 +0530

    mobile view fix

--- Mainpage/src/routes/LoginPage/Login.tsx
+++ Mainpage/src/routes/LoginPage/Login.tsx
@@ -6,7 +6,8 @@ const LoginMain = () => {
   return (
     <div className='container-design'>
       <div className='login-message'>
-        <h1>  Hello there, How you doing </h1>
+        <h1>  Hello there,  </h1>
+        <h1> How you doing </h1>
       </div>
       <Login/>
     </div>
