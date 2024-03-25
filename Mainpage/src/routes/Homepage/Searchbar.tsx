b1855df5e9600555483d48d5129ffb0269ba6913
nisarganag <nisarga.nag.cse24@heritageit.edu.in>
2024-03-25 14:06:12 +0530

    search added

--- Mainpage/src/routes/Homepage/Searchbar.tsx
+++ Mainpage/src/routes/Homepage/Searchbar.tsx
@@ -1,35 +1,47 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react';
 import { FaSearch } from "react-icons/fa";
-import './Searchbar.css'
+import './Searchbar.css';
 
-const searchbar = () => {
+const Searchbar = () => {
+    const [input, setInput] = useState("");
+    type SearchResultItem = {
+      title: string;
+      // other properties...
+  };
+    const [searchResult, setSearchResult] = useState<Array<SearchResultItem>>([]);
 
-  // const [input, setInput] = useState("")
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+      const newValue = event.target.value;
+      setInput(newValue);
+      fetchData(newValue);
+  };
+    const fetchData = async (query: string) => {
+      if (!query.trim()) {
+        setSearchResult([]);
+        return;
+    }
+        const response = await fetch(`http://localhost:8080/api/v1/publications/search?query=${query}`);
+        const data = await response.json();
+        setSearchResult(data);
+    };
 
-  // const fetchData = (value) => {
-  //   fetch("").then(Response) => Response.json();//api
+    
 
-        // return(
-        //   value &&
-        //   user &&
-        //   User.data.toLowercase().includes(value)
-        // )
-  // };
+    const handleSearchClick = () => {
+        fetchData(input);
+    };
 
-  // const handleChange= (value) => {
-  //   setInput(value);
-  //   fetchData(value);
-  // };
-
-
-
-  return (
-    <div className='input-component'>
-        <input placeholder='Type to Search... ' />
-        <FaSearch id='search-icon'/>
-
-    </div>
-  )
+    return (
+        <div className='input-component'>
+            <input placeholder='Type to Search... ' value={input} onChange={handleChange} />
+            <FaSearch id='search-icon' onClick={handleSearchClick} />
+            {searchResult && <div>{searchResult.map((item: SearchResultItem, index: number) => (
+                    <div key={index}>
+                        {item.title}
+                    </div>
+                ))}</div>}
+        </div>
+    );
 }
 
-export default searchbar
+export default Searchbar;
